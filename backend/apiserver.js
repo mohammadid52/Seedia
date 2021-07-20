0334ab8f002fedcba0e016eb1a4745955989eb2f
Mohammad <mohammadidehgamwala5@gmail.com>
2021-07-21 00:46:06 +0530

    backend change

--- backend/apiserver.js
+++ backend/apiserver.js
@@ -8,8 +8,6 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'build')))
-app.use(express.static(path.join(__dirname, 'build', 'static')))
 
 app.use('/login', loginRouter)
 
@@ -38,8 +36,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'build/static')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'))
 })
 
 app.listen(3005, () => {
