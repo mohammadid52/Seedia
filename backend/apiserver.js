dd1fff94155dd256ffb8b8aa9ecb0b81dc5523de
Mohammad <mohammadidehgamwala5@gmail.com>
2021-07-21 02:31:57 +0530

    static

--- backend/apiserver.js
+++ backend/apiserver.js
@@ -44,7 +44,7 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('static', express.static(path.join(__dirname, 'public')))
 
 app.listen(3005, () => {
     console.log('API Server running on 3005')
