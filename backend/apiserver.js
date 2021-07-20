a5cc8db0075e9401cccf0f7dd369a38c4a464c96
Mohammad <mohammadidehgamwala5@gmail.com>
2021-07-20 22:07:00 +0530

    added static files

--- backend/apiserver.js
+++ backend/apiserver.js
@@ -38,6 +38,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
+app.use(express.static(path.join(__dirname, 'build', 'path')))
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
