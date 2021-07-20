eea8a3255a7322fb97602960a075b60234b529b6
Mohammad <mohammadidehgamwala5@gmail.com>
2021-07-21 01:14:38 +0530

    bec

--- backend/apiserver.js
+++ backend/apiserver.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'))
 })
 
