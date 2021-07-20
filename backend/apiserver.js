6aed8d87b60bd08400bdf693147cbfd1fa68c12d
Mohammad <mohammadidehgamwala5@gmail.com>
2021-07-21 02:48:09 +0530

    bec

--- backend/apiserver.js
+++ backend/apiserver.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'build')))
-app.use(express.static(path.join(__dirname, 'build/static')))
+app.use(express.static(path.join(__dirname, 'build', 'static')))
 
 app.use('/login', loginRouter)
 
