0d2bf4f534317bd78009e1295f2c112d214a3eb0
Mohammad <mohammadidehgamwala5@gmail.com>
2021-07-21 00:50:17 +0530

    build changes

--- backend/apiserver.js
+++ backend/apiserver.js
@@ -43,6 +43,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'))
 })
 
+app.use('/', express.static(path.join(__dirname, 'build/static')))
+
 app.listen(3005, () => {
     console.log('API Server running on 3005')
 })
