cb570783ff639adff2fc6639b6c1ae581fac9921
Mohammad <mohammadidehgamwala5@gmail.com>
2021-07-21 21:48:25 +0530

    Fixed loader size

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -6,7 +6,7 @@ const Loading = ({ value }) => {
 
     return (
         <div className="h-screen w-screen flex items-center justify-center">
-            <img style={{height: '60%', width: '60%'}} src={'/logo-full.gif'} alt="Loading..." />
+            <img style={{width: '60%'}} src={'/logo-full.gif'} alt="Loading..." />
         </div>
     )
 }
