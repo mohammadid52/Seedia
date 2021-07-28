e7e5c95081ee47819b7524e0f233bea9acf96a29
Mohammad <mohammadidehgamwala5@gmail.com>
2021-07-28 23:15:11 +0530

    updated copyright component

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -7,21 +7,29 @@ const navigation = [
 const Copyright = () => {
   const year = new Date().getFullYear()
   return (
-    <div className="row absolute bottom-0 text-base w-full">
-      <hr />
-      <div className="light row mt-2">
-        <div className="text-center col-sm-6 col-md-12">
-          <p className="copy-right">
-            © {year} All rights reserved.{'  '}
-            {navigation.map((link) => (
-              <a href={link.href} className="link link-hover">
-                {link.name}
+    <footer className="">
+      <div className="max-w-7xl mx-auto pt-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+        <nav
+          className="-mx-5 lg:flex md:flex sm:grid sm:place-content-center sm:grid-cols-1 flex-wrap justify-center"
+          aria-label="Footer"
+        >
+          {navigation.map((item) => (
+            <div key={item.name} className="text-center px-5 py-2">
+              <a
+                href={item.href}
+                className="text-base text-gray-500 hover:text-gray-900"
+              >
+                {item.name}
               </a>
-            ))}
-          </p>
-        </div>
+            </div>
+          ))}
+        </nav>
+
+        <p className="mt-8 text-center text-base text-gray-400">
+          &copy; {year} Workflow, Inc. All rights reserved.
+        </p>
       </div>
-    </div>
+    </footer>
   )
 }
 
