7e56700aae3d7332df939323e28a3a9a71fe071a
Mohammad <mohammadidehgamwala5@gmail.com>
2021-08-04 01:22:54 +0530

    push buiild #5

--- backend/build/static/js/11.1ad8bb44.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp13rms=this.webpackJsonp13rms||[]).push([[11],{554:function(s,p){}}]);
-//# sourceMappingURL=11.1ad8bb44.chunk.js.map
---
+++ backend/build/static/js/11.b29e1cd4.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp13rms=this.webpackJsonp13rms||[]).push([[11],{534:function(s,p){}}]);
+//# sourceMappingURL=11.b29e1cd4.chunk.js.map
