7aff616e7e1036dbc9772b8f88922b5212424f5f
Mohammad <mohammadidehgamwala5@gmail.com>
2021-08-08 14:35:44 +0530

    removed unused components

--- src/components/DarkModeToggle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import Header from 'components/Header'
-
-const DarkModeToggle = ({ themeToggler, theme }) => {
-  return <Header themeToggler={themeToggler} theme={theme} />
-}
-
-export default DarkModeToggle
